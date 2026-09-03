c0786106139a98f9968c0ce36d9578c25c3012fb
VIGNESHKUMAR.SP <spvigneshkumar20@gmail.com>
2026-09-03 15:13:44 +0530

    Update sort.c

--- Student-Record-Management-System-in-C/sort.c
+++ Student-Record-Management-System-in-C/sort.c
@@ -1,88 +1,89 @@
 #include"header.h"
 
-void sort_by_name(SLL *ptr)
-{
-	SLL *p1,*p2;
-	int roll;
-	float percentage;
-	char name[50];
-
-	for(p1=ptr;p1;p1=p1->next)
-	{
-		for(p2=p1->next;p2;p2=p2->next)
-		{
-			if(strcmp(p1->name,p2->name)>0)
-			{
-				roll=p1->rollno;
-				p1->rollno=p2->rollno;
-				p2->rollno=roll;
-
-				strcpy(name,p1->name);
-				strcpy(p1->name,p2->name);
-				strcpy(p2->name,name);
-
-				percentage=p1->percentage;
-				p1->percentage=p2->percentage;
-				p2->percentage=percentage;
-			}
-		}
-	}
-}
-
-void sort_by_percentage(SLL *ptr)
-{
-	SLL *p1,*p2;
-	int roll;
-	float percentage;
-	char name[50];
-
-	for(p1=ptr;p1;p1=p1->next)
-	{
-		for(p2=p1->next;p2;p2=p2->next)
-		{
-			if(p1->percentage<p2->percentage)
-			{
-				roll=p1->rollno;
-				p1->rollno=p2->rollno;
-				p2->rollno=roll;
-
-				strcpy(name,p1->name);
-				strcpy(p1->name,p2->name);
-				strcpy(p2->name,name);
-
-				percentage=p1->percentage;
-				p1->percentage=p2->percentage;
-				p2->percentage=percentage;
-			}
-		}
-	}
-}
-
 void sort_student(SLL *ptr)
 {
-	char ch;
-
 	if(ptr==0)
 	{
 		printf("No records found\n");
 		return;
 	}
 
+	int c=count_node(ptr);
+	int i,j;
+	char ch;
+	SLL *p1,*p2,t;
+
 	printf("N/n : Sort with name\n");
 	printf("P/p : Sort with percentage\n");
 	printf("Enter choice: ");
 	scanf(" %c",&ch);
 
+	p1=ptr;
+
 	if(ch=='n' || ch=='N')
 	{
-		sort_by_name(ptr);
+		for(i=0;i<c-1;i++)
+		{
+			p2=p1->next;
+
+			for(j=0;j<c-1-i;j++)
+			{
+				if(strcmp(p1->name,p2->name)>0)
+				{
+					t.rollno=p1->rollno;
+					strcpy(t.name,p1->name);
+					t.percentage=p1->percentage;
+
+					p1->rollno=p2->rollno;
+					strcpy(p1->name,p2->name);
+					p1->percentage=p2->percentage;
+
+					p2->rollno=t.rollno;
+					strcpy(p2->name,t.name);
+					p2->percentage=t.percentage;
+				}
+
+				p2=p2->next;
+			}
+
+			p1=p1->next;
+		}
+
 		printf("Records sorted by name\n");
 	}
 	else if(ch=='p' || ch=='P')
 	{
-		sort_by_percentage(ptr);
+		for(i=0;i<c-1;i++)
+		{
+			p2=p1->next;
+
+			for(j=0;j<c-1-i;j++)
+			{
+				if(p1->percentage<p2->percentage)
+				{
+					t.rollno=p1->rollno;
+					strcpy(t.name,p1->name);
+					t.percentage=p1->percentage;
+
+					p1->rollno=p2->rollno;
+					strcpy(p1->name,p2->name);
+					p1->percentage=p2->percentage;
+
+					p2->rollno=t.rollno;
+					strcpy(p2->name,t.name);
+					p2->percentage=t.percentage;
+				}
+
+				p2=p2->next;
+			}
+
+			p1=p1->next;
+		}
+
 		printf("Records sorted by percentage\n");
 	}
 	else
+	{
 		printf("Invalid choice\n");
+	}
 }
